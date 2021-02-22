e8f60b2923907ff2f090dbf180f9b3c838aed9bb
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2021-02-22 12:55:01 -0300

    class 01 - install dependencies, set server, set routes

---
+++ api/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/users", (req, res) => {
+    return res.json({message: "Show me users please!!"});
+});
+
+app.post("/", (req, res) => {
+    // Recebeu dados para salvar
+    return res.json({message: "Success!!!"})
+});
+
+app.listen(3333, () => console.log('🖥  Server is running on port 3333'));

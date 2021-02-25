5edac93c536cf15999b2068c7780284d76869091
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2021-02-25 12:35:30 -0300

    Create the user migration, but is not migrating.

---
+++ api/src/database/migrations/1614253603059-createUsers.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createUsers1614253603059 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "name",
+                        type: "varchar"
+                    },
+                    {
+                        name: "email",
+                        type: "varchar"
+                    },
+                    {
+                        name: "createdAt",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
+    }
+
+}
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,14 +1,16 @@
+import 'reflect-metadata';
 import express from 'express';
+import './database';
 
 const app = express();
 
 app.get("/users", (req, res) => {
-    return res.json({message: "Show me users please!!"});
+    return res.json({ message: "Show me users please!!" });
 });
 
 app.post("/", (req, res) => {
     // Recebeu dados para salvar
-    return res.json({message: "Success!!!"})
+    return res.json({ message: "Success!!!" })
 });
 
 app.listen(3333, () => console.log('🖥  Server is running on port 3333'));
